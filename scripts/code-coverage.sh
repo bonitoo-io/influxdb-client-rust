781d54e4eb28cda420372c9a82e97b35323f63e2
Jakub Bednar <jakub.bednar@gmail.com>
2020-06-30 16:22:51 +0200

    docs: generate code coverage

--- scripts/code-coverage.sh
+++ scripts/code-coverage.sh
@@ -5,19 +5,14 @@ set -euo pipefail
 # https://github.com/codecov/example-rust
 #
 
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
-  tar xzf master.tar.gz &&
-  cd kcov-master &&
-  mkdir build &&
-  cd build &&
-  cmake .. &&
-  make &&
-  make install DESTDIR=../../kcov-build &&
-  cd ../.. &&
-  rm -rf kcov-master &&
-  cargo build &&
-  cargo test &&
-  ls target/debug &&
-  for file in target/debug/influxdb_client_rust-*[^\.d]; do [ -x "${file}" ] || continue; mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov-system-daemon --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
-  bash <(curl -s https://codecov.io/bash) &&
-  echo "Uploaded code coverage"
+exec docker run \
+  --rm \
+  --security-opt seccomp=unconfined \
+  --volume "${PWD}:/volume" \
+  xd009642/tarpaulin:latest-nightly \
+  cargo tarpaulin \
+    --all \
+    --verbose \
+    --exclude-files 'tests/*' \
+    --exclude-files 'main.rs' \
+    "$@"
